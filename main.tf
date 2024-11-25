fc3beebabf2ddfdd64f892ffb446bcde8f056690
prasanna <161597594+Prasannagummireddy@users.noreply.github.com>
2024-11-25 13:23:14 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ module "vpc" {
   version         = "5.16.0"
   name            = "Prasanna-VPC"
   cidr            = "10.0.0.0/16"
-  azs             = ["eu-central-1"]
+  azs             = ["eu-central-1a"]
   private_subnets = ["10.0.1.0/24"]
   public_subnets  = ["10.0.101.0/24"]
   public_subnet_tags = {
